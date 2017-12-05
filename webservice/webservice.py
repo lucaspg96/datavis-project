9a8d05da34040760a9377abfc34ef1211b30d487
Lucas Vasconcelos <lucasvasco8@gmail.com>
2017-12-04 23:53:22 -0200

    bug fix

--- webservice/webservice.py
+++ webservice/webservice.py
@@ -155,6 +155,6 @@ api.add_resource(ItensRestaurant, '/restaurant/itens/<string:id_place>/', endpoi
 
 api.add_resource(RestaurentsSameIten, '/restaurant/similar/<string:id_place>/', endpoint='restaurentsSimilar')
 
-api.add_resource(DishesStats, '/restaurant/similar/<string:attribute>/', endpoint='dishesStats')
+api.add_resource(DishesStats, '/restaurant/similar/<string:attribute>/<string:minimum>/<string:maximum>', endpoint='dishesStats')
 
 app.run(host='0.0.0.0', port=8000, debug=True)
