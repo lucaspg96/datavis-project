ff28be594209f591b91be6385c7be1b7025da5b0
Lucas Peres <lucasperes@insightlab.ufc.br>
2020-07-09 19:53:38 -0300

    Aplicando filtro temporal

---
+++ src/main/resources/webapp/precache-manifest.1b934775ce1e4cea06d9941c636cb32b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47da0456e90eaa59c1f950d250ed3144",
+    "url": "/datavis-project/index.html"
+  },
+  {
+    "revision": "367fdb467978acacd2d4",
+    "url": "/datavis-project/static/css/2.1bfbd074.chunk.css"
+  },
+  {
+    "revision": "76146e5263e184547acb",
+    "url": "/datavis-project/static/css/main.b12ace22.chunk.css"
+  },
+  {
+    "revision": "367fdb467978acacd2d4",
+    "url": "/datavis-project/static/js/2.0ed69c7a.chunk.js"
+  },
+  {
+    "revision": "f0a2002d1877a1632c1e02e2348a65a2",
+    "url": "/datavis-project/static/js/2.0ed69c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76146e5263e184547acb",
+    "url": "/datavis-project/static/js/main.3c68a999.chunk.js"
+  },
+  {
+    "revision": "a72de45b579276eff185",
+    "url": "/datavis-project/static/js/runtime-main.7769b6a8.js"
+  }
+]);
--- src/main/resources/webapp/service-worker.js
+++ src/main/resources/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/datavis-project/precache-manifest.d35d29ef91cec27e219c0df600381c53.js"
+  "/precache-manifest.42b4eccc66ae2e198da7eba7e3df58ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/datavis-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
