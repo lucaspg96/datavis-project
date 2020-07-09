0a1722d64a41d13967db83f57844a1b5f3d6509e
Lucas Peres <lucasperes@insightlab.ufc.br>
2020-07-09 15:17:24 -0300

    Tentando disponibilizar

--- src/main/resources/webapp/service-worker.js
+++ src/main/resources/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3db6cd6a9dc63982e3aa0587a987b3a.js"
+  "/datavis-project/precache-manifest.7db034e1bba6d778832a36aa2897703a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/datavis-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
