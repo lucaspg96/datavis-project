fe4544834a0eb2accdd3bead54456092764a04b4
Lucas Peres <lucasperes@insightlab.ufc.br>
2020-07-10 08:49:55 -0300

    Ajustando path

---
+++ precache-manifest.2339416e9d2e17b37df959e9715a3c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90274f47560651a49821a1fbbc41059",
+    "url": "/datavis-project/index.html"
+  },
+  {
+    "revision": "71173faef728bc531dd2",
+    "url": "/datavis-project/static/css/2.1bfbd074.chunk.css"
+  },
+  {
+    "revision": "a91cd06b6bfe896638ba",
+    "url": "/datavis-project/static/css/main.a50e11fc.chunk.css"
+  },
+  {
+    "revision": "71173faef728bc531dd2",
+    "url": "/datavis-project/static/js/2.04d178d6.chunk.js"
+  },
+  {
+    "revision": "f0a2002d1877a1632c1e02e2348a65a2",
+    "url": "/datavis-project/static/js/2.04d178d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91cd06b6bfe896638ba",
+    "url": "/datavis-project/static/js/main.950751fb.chunk.js"
+  },
+  {
+    "revision": "a72de45b579276eff185",
+    "url": "/datavis-project/static/js/runtime-main.7769b6a8.js"
+  }
+]);
