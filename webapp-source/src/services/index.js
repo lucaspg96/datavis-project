c73552d704291dd0118340f634832b990d6c57ae
Lucas Peres <lucasperes@insightlab.ufc.br>
2020-07-13 18:15:35 -0300

    Atualizando build da página no servidor

--- webapp-source/src/services/index.js
+++ webapp-source/src/services/index.js
@@ -4,5 +4,6 @@ const host = window.location.host
 const websocketURL = "ws://localhost:9000";
 
 const apiURL = "http://localhost:9000";
+// const apiURL = "http://" + host
 
 export { apiURL, websocketURL }
